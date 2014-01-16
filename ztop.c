cf42b6eaf9e95d6b9ea4df1f243b144e45257950
James Rowe <jroweboy@gmail.com>
2014-01-16 00:39:49 -0700

    Adding in several files from htop hoping that I will be able to use them soley for data collection and remove any cruft from it? Maybe it is a better idea to simply add a no display mode to htop?

---
+++ ztop.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+// #include <ncurses.h>
+#include <string.h> 
+
+// Stuff I've gathered from the Ncurses Tutorial
+//initscr(); // initialize the screen (clear it?)
+//cbreak() // command keys like ctrl-c will still send the kill sig but others will pass through
+//noecho(); // Do not output the users characters they type while in getch mode
+
+//printw("Hello World !!!");
+//refresh();
+//getch();
+//endwin(); // exit the curses screen mode
+
+typedef struct _fields {
+	
+} fields;
+
+int main(int argc, char **argv){
+
+	return 0;
+}
