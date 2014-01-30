d3feed61b8c395c482553f0014eac0e919fb8ddc
David P Hilton <david.hilton.p@gmail.com>
2014-01-29 17:16:42 -0700

    influxdb_setup.py: try teardown before setup

--- influxdb_setup.py
+++ influxdb_setup.py
@@ -5,6 +5,12 @@ from influxdb import client as influxdb
 db = influxdb.InfluxDBClient("localhost", 8086, "root", "root", "ztop")
 
 try:
+    db.delete_database("ztop")
+except Exception as e:
+    print(e)
+
+try:
     db.create_database("ztop")
 except Exception as e:
     print(e)
+
