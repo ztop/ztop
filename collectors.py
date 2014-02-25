11421af11dba2bd3dc7191b1cdfd219afe9d89a0
Brandon Weaver <brandon.weaver2@gmail.com>
2014-02-24 22:43:15 -0700

    Added more data for the system wide monitors

--- collectors.py
+++ collectors.py
@@ -1,17 +1,23 @@
 import psutil
-from systemMonitor import systemMonitorMemInfo
-from systemMonitor import systemMonitorNetworkUtilization
+from systemMonitor import *
 
-def cpu_percent():
-    values = psutil.cpu_times_percent()
-
+def convert_data_for_db_simple(dual_list):
     fields = []
     data = []
-    for field, point in zip(values._fields, values):
+    for field, point in dual_list:
         data.append(point)
         fields.append(field)
 
-    return fields, [data] # this data point doesn't have multiple rows; wrap in another
+    return fields, [data]
+
+def process_generic_monitor(className):
+    monitor = className()
+    monitor.update()
+    return convert_data_for_db_simple(monitor.fieldValues.iteritems())
+
+def cpu_percent():
+    values = psutil.cpu_times_percent()
+    return convert_data_for_db_simple(zip(values._fields, values))
 
 def network_usage():
     values = psutil.net_io_counters()
@@ -31,37 +37,106 @@ network_usage.last_values = []
 
 
 def mem_info():
-    memInfo = systemMonitorMemInfo()
-    memInfo.update()
-    keys = memInfo.fieldValues.keys()
-    values = memInfo.fieldValues.values()
-
-    fields = []
-    data = []
-
-    for field, point in memInfo.fieldValues.iteritems():
-        data.append(point)
-        fields.append(field)
-
-    return fields, [data]
+    return process_generic_monitor(systemMonitorMemInfo)
 
 def networkUtilization():
-    netDev = systemMonitorNetworkUtilization()
-    netDev.update()
-    keys = netDev.fieldValues.keys()
-    values = netDev.fieldValues.values()
+    return process_generic_monitor(systemMonitorNetworkUtilization)
 
-    fields = []
-    data = []
+def cpu_utilization():
+    return process_generic_monitor(systemMonitorCPUUtilization)
 
-    for field, point in netDev.fieldValues.iteritems():
-        data.append(point)
-        fields.append(field)
+def cpu_saturation():
+    return process_generic_monitor(systemMonitorCPUSaturation)
 
-    return fields, [data]
+def cpu_errors():
+    print "Not implemented"
 
+def memory_capacity_utilization():
+    return process_generic_monitor(systemMonitorMemoryCapacityUtilization)
+
+def memory_capacity_saturation():
+    return process_generic_monitor(systemMonitorMemoryCapacitySaturization)
+
+def memory_capacity_errors():
+    print "Not implemented"
+
+def network_interfaces_utilization():
+    print "Not implemented"
+
+def network_interfaces_saturation():
+    print "Not implemented"
+
+def network_interfaces_errors():
+    print "Not implemented"
+
+def storage_device_io_utilization():
+    print "Not implemented"
+
+def storage_device_io_saturation():
+    print "Not implemented"
+
+def storage_device_io_errors():
+    print "Not implemented"
+
+def storage_capacity_utilization():
+    print "Not implemented"
+
+def storage_capacity_saturation():
+    print "Not implemented"
+
+def storage_capacity_errors():
+    print "Not implemented"
+
+def storage_controller_utilization():
+    print "Not implemented"
+
+def storage_controller_saturation():
+    print "Not implemented"
+
+def storage_controller_errors():
+    print "Not implemented"
+
+def network_controller_utilization():
+    print "Not implemented"
+
+def network_controller_saturation():
+    print "Not implemented"
+
+def network_controller_errors():
+    print "Not implemented"
 
 collectors = {"cpu_percent": cpu_percent,
               "network_usage": network_usage,
               "mem_info": mem_info,
-              "net_dev": networkUtilization}
+              "net_dev": networkUtilization,
+              "cpu_utilization": cpu_utilization,
+              "cpu_saturation": cpu_saturation,
+              #"cpu_errors": cpu_errors, #requres perf
+              "memory_capacity_utilization": memory_capacity_utilization,
+              "memory_capacity_saturation": memory_capacity_saturation
+              #"memory_capacity_errors": memory_capacity_errors,
+              #"network_interfaces_utilization": network_interfaces_utilization,
+              #"network_interfaces_saturation": network_interfaces_saturation,
+              #"network_interfaces_errors": network_interfaces_errors,
+              #"storage_device_io_utilization": storage_device_io_utilization,
+              #"storage_device_io_saturation": storage_device_io_saturation,
+              #"storage_device_io_errors": storage_device_io_errors,
+              #"storage_capacity_utilization": storage_capacity_utilization,
+              #"storage_capacity_saturation": storage_capacity_saturation,
+              #"storage_capacity_errors": storage_capacity_errors,
+              #"storage_controller_utilization": storage_controller_utilization,
+              #"storage_controller_saturation": storage_controller_saturation,
+              #"storage_controller_errors": storage_controller_errors,
+              #"network_controller_utilization": network_controller_utilization,
+              #"network_controller_saturation": network_controller_saturation,
+              #"network_controller_errors": network_controller_errors,
+              #"cpu_interconnect_utilization": cpu_interconnect_utilization, #requires perf
+              #"cpu_interconnect_saturation": cpu_interconnect_saturation,  #requires perf
+              #"cpu_interconnect_errors": cpu_interconnect_errors,  #requires perf
+              #"memory_interconnect_utilization": memory_interconnect_utilization,  #requires perf
+              #"memory_interconnect_saturation": memory_interconnect_saturation,  #requires perf
+              #"memory_interconnect_errors": memory_interconnect_errors,  #requires perf
+              #"io_interconnect_utilization": io_interconnect_utilization,  #requires perf
+              #"io_interconnect_saturation": io_interconnect_saturation,  #requires perf
+              #"io_interconnect_errors": io_interconnect_errors,  #requires perf
+              }
--- systemMonitor.py
+++ systemMonitor.py
@@ -1,6 +1,7 @@
 import string
 import os
-
+import commands
+import time
 
 ''' All system monitors will use this interface '''
 class systemMonitor():
@@ -26,7 +27,7 @@ class systemMonitorMemInfo(systemMonitor):
 
 	#Updates the information we have from the meminfo file
 	#Each row of the file is in the following format 
-    #Name:        Int kB
+	#Name:        Int kB
 	def update(self):
 		meminfo = open('/proc/meminfo', 'r')
 		if meminfo:
@@ -99,7 +100,7 @@ class systemMonitorNetworkUtilization(systemMonitor):
 
 
 
-
+#TODO:
 class systemMonitorInterrupts(systemMonitor):
 
 	def __init__(self):
@@ -121,27 +122,124 @@ class systemMonitorInterrupts(systemMonitor):
 					print i
 
 
-''' For this CPU stats we will convert user_hz to seconds '''
+
+
+
+''' For this CPU stats we will convert user_hz to seconds 
+	The file format is the following
+	CPU usr nice sys iowait irq soft steal guest gnice idle
+	...
+No:	intr # # # # ... (x713 where the first is the total)
+	ctxt #
+	btime #
+	processes #
+	procs_running #
+	procs_blocked #
+	softirq # # # # # # # # # # #
+'''
 class systemMonitorCPUUtilization(systemMonitor):
 	#the /proc/stat file is in terms of user hz, which is most often 100
 	user_hz = 100
 
 	def __init__(self):
 		systemMonitor.__init__(self)
+		#this grabs the current architechture's user_hz
 		user_hz = os.sysconf(os.sysconf_names['SC_CLK_TCK'])
 
-	
+
 	def update(self):
-		print 'do something'
+		dataFile = open('/proc/stat', 'r')
+		if dataFile:
+			for line in dataFile:
+				print line[:3]
+				if line.startswith('cpu'):
+					localFields = string.split(line)
+					name = localFields[0]
+					self.fieldValues[name + '_usr'] = int(localFields[1]) / self.user_hz
+					self.fieldValues[name + '_nice'] = int(localFields[2]) / self.user_hz
+					self.fieldValues[name + '_sys'] = int(localFields[3]) / self.user_hz
+					self.fieldValues[name + '_iowait'] = int(localFields[4]) / self.user_hz
+					self.fieldValues[name + '_irq'] = int(localFields[5]) / self.user_hz
+					self.fieldValues[name + '_soft'] = int(localFields[6]) / self.user_hz
+					self.fieldValues[name + '_steal'] = int(localFields[7]) / self.user_hz
+					self.fieldValues[name + '_guest'] = int(localFields[8]) / self.user_hz
+					self.fieldValues[name + '_gnice'] = int(localFields[9]) / self.user_hz
+					self.fieldValues[name + '_idle'] = int(localFields[10]) / self.user_hz
+				elif line.startswith('intr'):
+					print 'Interrupt mapping not implemented for proc/stat'
+				elif line[:3] == 'softirq':
+					print 'Soft Interrupts mapping not implemented for proc/stat'
+				elif line.startswith('btime'):
+					localFields = string.split(line)
+					name = localFields[0]
+					self.fieldValues[name] = time.gmtime(float(localFields[1]))
+				else:
+					#The rest of the fields are straight numbers and don't need special treatment
+					localFields = string.split(line)
+					name = localFields[0]
+					self.fieldValues[name] = localFields[1]
 
-def test():
-	sysMon = systemMonitorMemInfo()
-	sysMon.update()
-	for key, value in sysMon.fieldValues.iteritems():
-		print key, '\t\t', value
 
-if __name__ == '__main__':
-	test()
+class systemMonitorCPUSaturation(systemMonitor):
+
+	def __init__(self):
+		systemMonitor.__init__(self)
 
 
+	def update(self):
+		cpu_count = commands.getoutput('cat /proc/cpuinfo | grep processor | wc -l')
+		data = commands.getoutput('vmstat')
+		lines = string.split(data, '\n')
+		procs_waiting = string.split(lines[-1])[0]
+		
+		self.fieldValues['cpu_count'] = cpu_count
+		self.fieldValues['cpu_saturation'] = float(procs_waiting)/float(cpu_count)
 
+
+class systemMonitorMemoryCapacityUtilization(systemMonitor):
+	def __init__(self):
+		systemMonitor.__init__(self)
+
+	#Updates the information we have from the meminfo file
+	#Each row of the file is in the following format 
+	#Name:        Int kB
+	def update(self):
+		meminfo = open('/proc/meminfo', 'r')
+		if meminfo:
+			for line in meminfo:
+				words = string.split(line)
+				if len(words) > 2:
+					name = words[0][:-1]
+					self.fieldValues[name] = words[1] + words[2]
+
+
+class systemMonitorMemoryCapacitySaturization(systemMonitor):
+	def __init__(self):
+		systemMonitor.__init__(self)
+
+	#Here we use the measure of data swapped in from disk and swapped to disk to measure saturation
+	#once we hit capacity we have to swap out memory to disk to make room for more data
+	#a good measure of saturation then is the ratio of amount swapped in to the amount swapped out
+	def update(self):
+		data = commands.getoutput('vmstat')
+		lines = string.split(data, '\n')
+		data_line = string.split(lines[-1])
+
+		#data should be in the following format:
+		#procs -----------memory---------- ---swap-- -----io---- -system-- ----cpu----
+		# r  b   swpd   free   buff  cache   si   so    bi    bo   in   cs us sy id wa
+
+		self.fieldValues['swapped_in'] = int(data_line[6])
+		self.fieldValues['swapped_out'] = int(data_line[7])
+		#check for zero denominator
+		saturation = 0
+		if (int(data_line[7]) > 0):
+			saturation = swapped_in / swapped_out
+		self.fieldValues['saturation'] = saturation
+
+class systemMonitorNetworkInterfacesUtilization(systemMonitor):
+	def __init__(self):
+		systemMonitor,__init__(self)
+
+	def update(self):
+		infoFile = open('/proc/net/dev', 'r')
