94de23674580de13aa99db524e88c827096b5d21
Brandon Weaver <brandon.weaver2@gmail.com>
2014-03-03 17:18:41 -0700

    Dropped the commas down to the next line so commening out lines doesn't require fiddling with commas

--- collectors.py
+++ collectors.py
@@ -37,42 +37,42 @@ network_usage.last_values = []
 
 
 
-collectors = {"cpu_percent": [cpu_percent],
-              "mem_info": [process_generic_monitor, systemMonitorMemInfo],
-              "cpu_utilization": [process_generic_monitor, systemMonitorCPUUtilization],
-              "cpu_saturation": [process_generic_monitor, systemMonitorCPUSaturation],
-              #"cpu_errors": cpu_errors, #requres perf
-              "memory_capacity_utilization": [process_generic_monitor, systemMonitorMemoryCapacityUtilization],
-              "memory_capacity_saturation": [process_generic_monitor, systemMonitorMemoryCapacitySaturization],
-              #"memory_capacity_errors": memory_capacity_errors,
-              "network_interfaces_utilization": [network_usage],
-              #"network_interfaces_saturation": network_interfaces_saturation, #information grabbed in network usage call
-              #"network_interfaces_errors": network_interfaces_errors,          #information grabbed in network usage call
-              "storage_device_io_utilization": [process_generic_monitor, systemMonitorStorageDeviceIOUtilization],
-              "storage_device_io_saturation": [process_generic_monitor, systemMonitorStorageDeviceIOSaturation],
-              #"storage_device_io_errors": storage_device_io_errors,
-              "storage_capacity_utilization": [process_generic_monitor, systemMonitorStorageCapacityUtilization],
-              #"storage_capacity_saturation": storage_capacity_saturation, #information grabbed in storage_capacity_utilization
-              #"storage_capacity_errors": storage_capacity_errors,
+collectors = {"cpu_percent": [cpu_percent]
+              ,"mem_info": [process_generic_monitor, systemMonitorMemInfo]
+              ,"cpu_utilization": [process_generic_monitor, systemMonitorCPUUtilization]
+              ,"cpu_saturation": [process_generic_monitor, systemMonitorCPUSaturation]
+              #,"cpu_errors": cpu_errors #requres perf
+              ,"memory_capacity_utilization": [process_generic_monitor, systemMonitorMemoryCapacityUtilization]
+              ,"memory_capacity_saturation": [process_generic_monitor, systemMonitorMemoryCapacitySaturization]
+              #,"memory_capacity_errors": memory_capacity_errors #not sure how to get this
+              ,"network_interfaces_utilization": [network_usage]
+              #,"network_interfaces_saturation": network_interfaces_saturation #information grabbed in network usage call
+              #,"network_interfaces_errors": network_interfaces_errors          #information grabbed in network usage call
+              ,"storage_device_io_utilization": [process_generic_monitor, systemMonitorStorageDeviceIOUtilization]
+              ,"storage_device_io_saturation": [process_generic_monitor, systemMonitorStorageDeviceIOSaturation]
+              #,"storage_device_io_errors": storage_device_io_errors #not sure how to get this
+              ,"storage_capacity_utilization": [process_generic_monitor, systemMonitorStorageCapacityUtilization]
+              #,"storage_capacity_saturation": storage_capacity_saturation #information grabbed in storage_capacity_utilization
+              #,"storage_capacity_errors": storage_capacity_errors
               
               #The following need max values per device/card which I'm not sure how to get
 
-              #"storage_controller_utilization": storage_controller_utilization,
-              #"storage_controller_saturation": storage_controller_saturation,
-              #"storage_controller_errors": storage_controller_errors,
-              #"network_controller_utilization": network_controller_utilization,
-              #"network_controller_saturation": network_controller_saturation,
-              #"network_controller_errors": network_controller_errors,
+              #,"storage_controller_utilization": storage_controller_utilization
+              #,"storage_controller_saturation": storage_controller_saturation
+              #,"storage_controller_errors": storage_controller_errors
+              #,"network_controller_utilization": network_controller_utilization
+              #,"network_controller_saturation": network_controller_saturation
+              #,"network_controller_errors": network_controller_errors
 
               #The following need perf
 
-              #"cpu_interconnect_utilization": cpu_interconnect_utilization, #requires perf
-              #"cpu_interconnect_saturation": cpu_interconnect_saturation,  #requires perf
-              #"cpu_interconnect_errors": cpu_interconnect_errors,  #requires perf
-              #"memory_interconnect_utilization": memory_interconnect_utilization,  #requires perf
-              #"memory_interconnect_saturation": memory_interconnect_saturation,  #requires perf
-              #"memory_interconnect_errors": memory_interconnect_errors,  #requires perf
-              #"io_interconnect_utilization": io_interconnect_utilization,  #requires perf
-              #"io_interconnect_saturation": io_interconnect_saturation,  #requires perf
-              #"io_interconnect_errors": io_interconnect_errors,  #requires perf
+              #,"cpu_interconnect_utilization": cpu_interconnect_utilization #requires perf
+              #,"cpu_interconnect_saturation": cpu_interconnect_saturation  #requires perf
+              #,"cpu_interconnect_errors": cpu_interconnect_errors  #requires perf
+              #,"memory_interconnect_utilization": memory_interconnect_utilization  #requires perf
+              #,"memory_interconnect_saturation": memory_interconnect_saturation  #requires perf
+              #,"memory_interconnect_errors": memory_interconnect_errors  #requires perf
+              #,"io_interconnect_utilization": io_interconnect_utilization  #requires perf
+              #,"io_interconnect_saturation": io_interconnect_saturation  #requires perf
+              #,"io_interconnect_errors": io_interconnect_errors  #requires perf
               }
